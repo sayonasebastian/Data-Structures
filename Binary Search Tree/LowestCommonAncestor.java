a836870acf23b989854785159b29f90e889ab424
sayonasebastian <53229496+sayonasebastian@users.noreply.github.com>
2023-01-28 01:48:00 -0500

    Add files via upload

---
+++ Binary Search Tree/LowestCommonAncestor.java
@@ -0,0 +1,13 @@
+package BST;
+// 235. Lowest Common Ancestor of a Binary Search Tree
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
